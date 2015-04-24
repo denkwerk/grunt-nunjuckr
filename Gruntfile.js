a8a290ccfba2f96fbb51439081478c28e48af1cf
Marius Büscher <marius.buescher@denkwerk.com>
2015-04-24 09:08:47 +0200

    [~TASK] Fixed indentation

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,62 +1,62 @@
 module.exports = function (grunt) {
 
-  var path = require('path');
+    var path = require('path');
 
-  grunt.initConfig({
+    grunt.initConfig({
 
-    nunjuckr : {
+        nunjuckr : {
 
-      testSimple : {
-        options : {
-          data : grunt.file.readJSON('test/simple/data/data.json')
-        },
-        files : [
-          {
-            src : 'test/simple/src/index.njs',
-            dest : 'test/simple/dest/index.html'
-          }
-        ]
-      },
-      testExtended : {
-        options : {
-          data : grunt.file.readJSON('test/extended/data/data.json'),
-          ext : '.html',
-          tags : {
-            blockStart: '<%',
-            blockEnd: '%>',
-            variableStart: '<$',
-            variableEnd: '$>',
-            commentStart: '<#',
-            commentEnd: '#>'
-          },
-          searchPaths : 'test/extended/src',
-          setUp : function (env) {
-            env.addFilter('crop', function (str, count) {
-              return str.slice(0, count || 5);
-            });
-            return env;
-          },
-          preprocessData : function (data, file) {
-            var fileExt = path.extname(file);
-            var filename = path.basename(file, fileExt);
-            var dir = path.dirname(file);
-            var jsonPath = path.join('test/extended/data/', dir, filename + '.json');
+            testSimple : {
+                options : {
+                    data : grunt.file.readJSON('test/simple/data/data.json')
+                },
+                files : [
+                    {
+                        src : 'test/simple/src/index.njs',
+                        dest : 'test/simple/dest/index.html'
+                    }
+                ]
+            },
+            testExtended : {
+                options : {
+                    data : grunt.file.readJSON('test/extended/data/data.json'),
+                    ext : '.html',
+                    tags : {
+                        blockStart: '<%',
+                        blockEnd: '%>',
+                        variableStart: '<$',
+                        variableEnd: '$>',
+                        commentStart: '<#',
+                        commentEnd: '#>'
+                    },
+                    searchPaths : 'test/extended/src',
+                    setUp : function (env) {
+                        env.addFilter('crop', function (str, count) {
+                            return str.slice(0, count || 5);
+                        });
+                        return env;
+                    },
+                    preprocessData : function (data, file) {
+                        var fileExt = path.extname(file);
+                        var filename = path.basename(file, fileExt);
+                        var dir = path.dirname(file);
+                        var jsonPath = path.join('test/extended/data/', dir, filename + '.json');
 
-            data = grunt.file.readJSON(jsonPath);
+                        data = grunt.file.readJSON(jsonPath);
 
-            return data;
-          }
-        },
-        files : [
-          {
-            src : 'test/extended/src/**/*.njs',
-            dest : 'test/extended/dest/'
-          }
-        ]
-      }
-    }
+                        return data;
+                    }
+                },
+                files : [
+                    {
+                        src : 'test/extended/src/**/*.njs',
+                        dest : 'test/extended/dest/'
+                    }
+                ]
+            }
+        }
 
-  });
+    });
 
-  grunt.loadTasks('tasks');
+    grunt.loadTasks('tasks');
 };
